db296a300c3e724af4d8ca5c91ea76acd4b986d9
Kylor Hall <kylor@kylorhall.com>
2021-12-05 09:33:11 +1300

    Add another test case.

--- src/file.ts
+++ src/file.ts
@@ -3,3 +3,7 @@ console.log(`In environment: ${GLOBAL_SETTINGS.ENV}`);
 if (GLOBAL_SETTINGS.DEBUG) {
   console.debug('Debug enabled');
 }
+
+if (OTHER_GLOBAL_TYPE) {
+  console.debug('Other global enabled');
+}
